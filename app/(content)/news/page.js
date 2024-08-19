c0c9e974cf3f053f711fb6d760fc8896949572be
zohaib <zohaibshafiq2005@gmail.com>
2024-08-19 12:17:44 +0500

    data fetching in next

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,11 +1,19 @@
-import { DUMMY_NEWS } from '@/dummy-news';
-import NewsList from '@/components/news-list';
+// import { DUMMY_NEWS } from "@/dummy-news";
+import NewsList from "@/components/news-list";
 
-export default function NewsPage() {
+export default async function NewsPage() {
+  const response = await fetch("http://localhost:8080/news");
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch news.");
+  }
+
+  const news = await response.json();
+
   return (
     <>
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      <NewsList news={news} />
     </>
   );
 }
